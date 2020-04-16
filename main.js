3772230d8e4feabf87b10e0df28b9bdd9cc7526d
PilarCha <pchavez92@gmail.com>
2020-04-15 20:34:52 -0700

    More computed properties

--- main.js
+++ main.js
@@ -5,20 +5,21 @@ var app = new Vue ({
     product: 'Socks',
     details: ["80% Cotton","20% Polyester","Machine-Washable"],
     link: 'https://www.google.com/',
-    image:'./images/green-socks.jpg',
-    inStock: false,
+    selectedVariant:0,
     inventory: -1,
     onSale: true,
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './images/green-socks.jpg'
+        variantImage: './images/green-socks.jpg',
+        variantQty: 10
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './images/blue-socks.jpg'
+        variantImage: './images/blue-socks.jpg',
+        variantQty: 0
       }
     ],
     sizes: ["SM","MED","LRG","XL","XXXXXL"],
@@ -28,16 +29,27 @@ var app = new Vue ({
     addToCart() {
       this.cart += 1
     },
-    updateProduct(variantImage) {
-      this.image = variantImage
+    updateProduct(index) {
+      this.selectedVariant = index;
+      console.log(index)
     },
     removeFromCart() {
       this.cart == 0 ? this.cart = 0 : this.cart -= 1
     }
   },
+  //cached for more of a heavy load
   computed: {
     title() {
       return this.brand + ' ' +this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQty
+    },
+    checkSale() {
+      if (this.onSale) return `${this.brand}  ${this.product} is on sale!`
     }
   }
 })
